16d6c2acb2ef2a12bc826702ae6f41ae9930426f
Wojciech Osmulski <34412964+wojciechosmulski@users.noreply.github.com>
2023-12-30 15:56:29 +0100

    feat: create country card component (#9)

---
+++ travel-app/app/index.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import { View, Text } from './../components/Themed'
+import { StyleSheet } from 'react-native'
+import axios from 'axios'
+import CountryCard from './../components/CountryCard'
+
+type Country = {
+  name: string
+  image: string
+}
+
+const Index = () => {
+  const [countries, setCountries] = useState<Country[]>([])
+
+  useEffect(() => {
+    axios
+      .get('http://192.168.1.14:3000/api/countries')
+      .then((response) => {
+        setCountries(response.data)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Countries</Text>
+      {countries.map((country) => (
+        <CountryCard
+          key={country.name}
+          countryName={country.name}
+          imageUrl={country.image}
+        />
+      ))}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+})
+
+export default Index
