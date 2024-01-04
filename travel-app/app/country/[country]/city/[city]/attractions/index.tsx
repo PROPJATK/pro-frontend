ac85395dfc96feaf5a675d9581fd276e243c0ebf
Wojciech Osmulski <34412964+wojciechosmulski@users.noreply.github.com>
2024-01-04 17:50:50 +0100

    feat: finalize routing and api fetching (#14)

---
+++ travel-app/app/country/[country]/city/[city]/attractions/index.tsx
@@ -0,0 +1,88 @@
+import { useEffect, useState } from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native'
+import {
+  router,
+  useNavigation,
+  usePathname,
+} from 'expo-router'
+
+type AttractionData = {
+  id: string
+  _id: string
+  name: string
+  link: string
+  address: string
+}
+
+const AttractionsPage = () => {
+  const [attractionsData, setAttractionsData] = useState<
+    AttractionData[] | null
+  >(null)
+  const pathname = usePathname()
+  const countryName = pathname.split('/')[2]
+  const cityName = pathname.split('/')[4]
+
+  useEffect(() => {
+    const fetchAttractionsData = async () => {
+      const response = await fetch(
+        `http://192.168.1.14:3000/api/countries/${countryName}/cities/${cityName}/attractions`
+      )
+      const data = await response.json()
+      setAttractionsData(data)
+    }
+    fetchAttractionsData()
+  }, [])
+
+  if (!attractionsData) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Loading...</Text>
+      </View>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      {attractionsData.map((attraction: AttractionData) => (
+        <View key={attraction._id} style={styles.card}>
+          <Text style={styles.title}>
+            {attraction.name}
+          </Text>
+          <Text style={styles.address}>
+            {attraction.address}
+          </Text>
+        </View>
+      ))}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    gap: 10,
+    alignItems: 'center',
+  },
+  card: {
+    backgroundColor: '#363636',
+    padding: 8,
+    margin: 8,
+    borderRadius: 4,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  address: {
+    color: '#fff',
+    fontSize: 14,
+  },
+})
+
+export default AttractionsPage
---
+++ travel-app/app/country/[country]/city/[city]/hotels/index.tsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import { router, usePathname } from 'expo-router'
+
+type HotelData = {
+  id: string
+  _id: string
+  name: string
+  link: string
+  address: string
+  stars: number
+}
+
+const HotelsPage = () => {
+  const [hotelsData, setHotelsData] = useState<
+    HotelData[] | null
+  >(null)
+  const pathname = usePathname()
+  const countryName = pathname.split('/')[2]
+  const cityName = pathname.split('/')[4]
+
+  useEffect(() => {
+    const fetchHotelsData = async () => {
+      const response = await fetch(
+        `http://192.168.1.14:3000/api/countries/${countryName}/cities/${cityName}/hotels`
+      )
+      const data = await response.json()
+      setHotelsData(data)
+    }
+    fetchHotelsData()
+  }, [])
+
+  if (!hotelsData) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Loading...</Text>
+      </View>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      {hotelsData.map((hotel: HotelData) => (
+        <View key={hotel._id} style={styles.card}>
+          <Text style={styles.title}>{hotel.name}</Text>
+          <Text style={styles.address}>
+            {hotel.address}
+          </Text>
+          <Text style={styles.address}>
+            {hotel.stars} stars
+          </Text>
+        </View>
+      ))}
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    gap: 10,
+    alignItems: 'center',
+  },
+  card: {
+    backgroundColor: '#363636',
+    padding: 8,
+    margin: 8,
+    borderRadius: 4,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  address: {
+    color: '#fff',
+    fontSize: 14,
+  },
+})
+
+export default HotelsPage
